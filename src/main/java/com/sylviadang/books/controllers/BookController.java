705d8be92ef995b14369b6751db9914f4116532e
Your Name <email@example.com>
2023-08-09 19:22:31 -0700

    all books

--- src/main/java/com/sylviadang/books/controllers/BookController.java
+++ src/main/java/com/sylviadang/books/controllers/BookController.java
@@ -1,11 +1,15 @@
 package com.sylviadang.books.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.sylviadang.books.models.Book;
 import com.sylviadang.books.services.BookService;
 
 @Controller
@@ -19,6 +23,7 @@ public class BookController {
 		return "redirect:/books";
 	}
 	
+	
 	@GetMapping("/books/{bookId}")
 	public String index(Model model, @PathVariable("bookId") Long bookId) {
 		
@@ -26,4 +31,10 @@ public class BookController {
 		return "show.jsp";
 	}
 	
+	@RequestMapping("/books")
+	public String index(Model model) {
+		List<Book> books = service.allBooks();
+		model.addAttribute("books", books);
+		return "/books/index.jsp";
+	}
 }
---
+++ src/main/webapp/WEB-INF/index.jsp
@@ -0,0 +1,34 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<%@ taglib prefix = "c" uri = "http://java.sun.com/jsp/jstl/core" %>
+    
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+<h1>All Books</h1>
+<table>
+	<thead>
+		<tr>
+			<th>ID</th>
+			<th>Title</th>
+			<th>Language</th>
+			<th># Pages</th>
+		</tr>
+	</thead>
+	<tbody>
+         <c:forEach var="book" items="${books}">
+         	<tr>
+         		<td><c:out value="${book.id}"/></td>
+         		<td><c:out value="${book.title}"/></td>
+         		<td><c:out value="${book.language}"/></td>
+         		<td><c:out value="${book.numberOfPages}"/></td>
+         	</tr>
+         </c:forEach>
+    </tbody>
+</table>
+</body>
+</html>
