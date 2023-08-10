5f8a66f4b93102ac135b6d79ffe38b0c61254256
Your Name <email@example.com>
2023-08-09 19:02:46 -0700

    rendering books

--- src/main/java/com/sylviadang/books/controllers/BookController.java
+++ src/main/java/com/sylviadang/books/controllers/BookController.java
@@ -1,55 +1,29 @@
 package com.sylviadang.books.controllers;
 
-import java.util.List;
-
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
-import com.sylviadang.books.models.Book;
 import com.sylviadang.books.services.BookService;
 
-@RestController
+@Controller
 public class BookController {
+	@Autowired
+	BookService service;
 	
-	private final BookService service;
 	
-	public BookController(BookService service) {
-		this.service = service;
+	@GetMapping("/")
+	public String home() {
+		return "redirect:/books";
 	}
 	
-	@RequestMapping("/api/books")
-	public List<Book> index() {
-		return service.allBooks();
+	@GetMapping("/books/{bookId}")
+	public String index(Model model, @PathVariable("bookId") Long bookId) {
+		
+		model.addAttribute("book", service.findBook(bookId));
+		return "show.jsp";
 	}
 	
-	@RequestMapping(value="/api/books", method=RequestMethod.POST)
-	public Book create(
-			@RequestParam(value="title") String title, 
-			@RequestParam(value="description") String description, 
-			@RequestParam(value="language") String lang, 
-			@RequestParam(value="pages") Integer numOfPages){
-		Book book = new Book(title, description, lang, numOfPages);
-		return service.createBook(book);
-	}
-	
-	@RequestMapping("/api/books/{id}")
-	public Book show(@PathVariable("id") Long id) {
-		Book book = service.findBook(id);
-		return book;
-	}
-	
-	@RequestMapping(value="/api/books/{id}", method=RequestMethod.PUT)
-    public Book updateBook(@PathVariable("id") Long id, @RequestParam(value="title") String title, @RequestParam(value="description") String description, @RequestParam(value="language") String lang, @RequestParam(value="pages") Integer numOfPages) {
-        Book book = new Book(title, description, lang, numOfPages);
-        book.setId(id);
-        return service.updateBook(book);
-    }
-    
-    @RequestMapping(value="/api/books/{id}", method=RequestMethod.DELETE)
-    public void deleteBook(@PathVariable("id") Long id) {
-        service.deleteBook(id);
-    }
 }
